ff560465a8b10dac4b5de809b32c92c53911042e
Gabriel Duarte <gabriellagoa10@yahoo.com.br>
2015-09-09 11:15:45 -0300

    Codigo classe inicial

--- GoldBit_SensorCorrente.h
+++ GoldBit_SensorCorrente.h
@@ -1,11 +1,25 @@
 #ifndef GoldBit_SensorCorrente_h
 #define GoldBit_SensorCorrente_h
 #include "Arduino.h"
-class GoldBit_SensorCorrente
+
+#define ACS722 1
+
+class Sensor
 {
-public: 
-  
-private:
+	public: 
+	  Sensor(int _ID, double _start = 100, double _end = 500)
+	  {
+	  	ID = _ID;
+	  	start = _start;
+		end = _end;
+	  }
+	  bool getState()
+	  {
+	  	return 1;
+	  }
+	private:
+		int ID;
+		double start, end;
 };
 #endif
 
